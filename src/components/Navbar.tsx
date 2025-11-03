234a7227cf250f85aa4a8736120ecc9c20d84d54
LEARN AS U GROW 501 <46976270+LAUG501@users.noreply.github.com>
2025-11-02 19:10:34 -0500

    Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,7 +19,6 @@ export default function Navbar() {
     { href: '/event-calendar', label: 'Events' },
     { href: '/blog', label: 'Blog' },
     { href: '/about-us', label: 'About' },
-    { href: '/about-us/juana-diaz', label: 'History' },
     { href: '/contact', label: 'Contact' }
   ];
 
